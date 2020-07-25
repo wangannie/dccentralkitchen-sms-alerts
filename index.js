0be1aa84ff66c2190bced01156c20977e7469c43
Annie Wang <anniedwang02@gmail.com>
2020-07-25 11:17:32 -0400

    Add send_sms calls

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import config from "./config.js";
 import { notifyCustomers } from "./lib/utils/twilioNotifications.js";
+import { sendSms } from "./twilioClient.js";
 
 // Create Express web app
 var app = express();
@@ -27,6 +28,50 @@ app.post("/send_alert", async (req, res) => {
     res.send(
       "Success! Delivery alerts were sent to " + customers + " customer(s)."
     );
+  } catch (err) {
+    console.error(err);
+    res.send(`<h1>Error</h1>
+    <p>${err}</p>`);
+  }
+});
+
+app.post("/send_sms", async (req, res) => {
+  const secretKey = req.body.key;
+  const to = req.body.to;
+  console.log(body);
+  const message = req.body.message;
+
+  if (secretKey !== process.env.HC_SECRET) {
+    res.send(`<h1>Error: usage of this API requires a secret key</h1>
+    <p>Please notify someone to help you get access.</p>`);
+    return;
+  }
+  try {
+    sendSms(to, message);
+    res.send("Success! Your message '" + message + "' was sent to " + to);
+  } catch (err) {
+    console.error(err);
+    res.send(`<h1>Error</h1>
+    <p>${err}</p>`);
+  }
+});
+
+app.post("/send_sms_batch", async (req, res) => {
+  const secretKey = req.body.key;
+  const to = req.body.to;
+  console.log(req.body);
+  const message = req.body.message;
+
+  if (secretKey !== process.env.HC_SECRET) {
+    res.send(`<h1>Error: usage of this API requires a secret key</h1>
+    <p>Please notify someone to help you get access.</p>`);
+    return;
+  }
+  try {
+    to.forEach(function (recipient) {
+      sendSms(recipient, message);
+    });
+    res.send("Success! '" + message + "' was sent to " + to.join(", "));
   } catch (err) {
     console.error(err);
     res.send(`<h1>Error</h1>
